981244afcef89cdc1703408653b452c06f6858ac
abhinav33agrawal <abhinav22agrawal@gmail.com>
2021-04-28 02:00:41 +0530

    Search header completed

---
+++ src/SideBar.js
@@ -0,0 +1,36 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@material-ui/icons";
+import React from "react";
+import "./SideBar.css";
+function SideBar() {
+  return (
+    <div className="sidebar">
+    <div className="sidebar__header">
+        <Avatar/>
+        <div className="sidebar__headerRight">
+        <IconButton>
+                <DonutLarge/>
+        </IconButton>
+        <IconButton>
+                <Chat/>
+        </IconButton>
+        <IconButton>
+                <MoreVert/>
+        </IconButton>
+        </div>
+    </div>
+    <div className="sidebar__search">
+        <div className="sidebar__searchContainers">
+
+        <SearchOutlined/>
+        <input placeholder = 'Start a new chat'/>
+        </div>
+    </div>
+    <div className="sidebar__chats">
+
+    </div>
+    </div>
+  );
+}
+
+export default SideBar;
